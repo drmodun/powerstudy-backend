e8d323314464604ffd4a34cb333c50784026d897
drmodun <jan.modun.st@gmail.com>
2024-12-16 03:14:54 +0100

    fix page params

--- src/base/query/BaseQuery.ts
+++ src/base/query/BaseQuery.ts
@@ -1,8 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
 import { IsNumber, IsOptional, Max, Min } from 'class-validator';
 
 export class BaseQuery {
   @ApiProperty({ required: false })
+  @Transform(({ value }) => parseInt(value))
+  @Type(() => Number)
   @IsOptional()
   @IsNumber()
   @Min(1)
@@ -10,6 +13,8 @@ export class BaseQuery {
 
   @ApiProperty({ required: false })
   @IsOptional()
+  @Transform(({ value }) => parseInt(value))
+  @Type(() => Number)
   @IsNumber()
   @Min(1)
   @Max(50)
